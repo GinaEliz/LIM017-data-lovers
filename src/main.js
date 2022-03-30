db179f87676efab8e95dfe657ed07ff0764641ca
Areliz Oyarce <arelizuwc@gmail.com>
2022-03-30 04:32:35 -0500

    adding modals styles and modal data

--- src/main.js
+++ src/main.js
@@ -7,25 +7,52 @@ const championsData = Object.values(data.data);
 //MOSTRAR LA DATA
 function seeChampions(championsData) {
     const html = document.getElementById("card");
-    let infoHTML = '';
+    const modalsHtml = document.getElementById("modals");
+    let seeDataHTML = '';
+    let seeModalDataHtml = '';
     for (let champion in championsData) {
         let data_champion = championsData[champion];
-        infoHTML += `<section class="champions" id="champions">
-        <a href=".infochampions"><img class="img-champions" id="img_champions"src=${data_champion.splash}></a>
+        seeDataHTML += `<section class="champions" id="champions">
+        <img class="img-champions" data-champ="${data_champion.id}"src=${data_champion.splash}>
         <p class="name-champions"> ${data_champion.id}</p>
-        </section>
-        <section class="infochampions" style="display:none";> 
-        <p>Título: ${data_champion.title}</p>
-        <p>Descripción: ${data_champion.blurb}</p>
-        <p>Información:</p>
-        <p> Ataque: ${data_champion.info.attack}</p>
-        <p> Defensa:${data_champion.info.defense}</p>
-        <p> Magia: ${data_champion.info.magic}</p>
-        <p> Dificultad: ${data_champion.info.difficulty}</p>
         </section>`
+        seeModalDataHtml += `<section class="info_champions" id="${data_champion.id}" style="display:none;">
+        <div id="box_content">
+        <span class="info_champions_close" data-close="${data_champion.id}">x</span>
+        <img class="backgroung_img_champion" data-champ="${data_champion.id}"src=${data_champion.splash}>
+        <p class="modal_name_champion"> ${data_champion.id}</p>
+        <p class="modal_description_champion">${data_champion.blurb}</p>
+        <section class="modal_description_features">
+        <p> Ataque:</p>
+        <p> Defensa:</p>
+        <p> Magia:</p>
+        <p> Dificultad:</p>
+        </section>
+        <section class="modal_features">
+        <p>${data_champion.info.attack}</p>
+        <p>${data_champion.info.defense}</p>
+        <p>${data_champion.info.magic}</p>
+        <p>${data_champion.info.difficulty}</p>
+        </section>
+        </div>
+        </section>`
+        html.innerHTML = seeDataHTML;
+        modalsHtml.innerHTML = seeModalDataHtml;
     }
-    html.innerHTML = infoHTML;
-}
-seeChampions(championsData);
 
-//hola
+
+    const openModal = document.querySelectorAll(".img-champions")
+    const hideModal = document.querySelectorAll(".info_champions_close");
+    openModal.forEach(om => {
+        om.addEventListener('click', e => {
+            document.getElementById(e.target.dataset.champ).style.display = "block";
+        })
+    })
+    hideModal.forEach(hm => {
+        hm.addEventListener('click', e => {
+            document.getElementById(e.target.dataset.close).style.display = "none";
+        })
+    })
+}
+
+seeChampions(championsData);
